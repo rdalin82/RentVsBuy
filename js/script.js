c42f24232ca3d637335e8b9dd9cfd72ef4e4d7cb
rdalin82 <Robert.Dalin@gmail.com>
2016-04-26 17:04:22 -0500

    working componet

---
+++ js/script.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+
+class Layout extends React.Component {
+	render(){
+		return (
+			<p>Do it!</p>
+		);
+	}
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);
--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,23 @@ var webpack = require('webpack');
 module.exports = {
   context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/scripts.js",
+  entry: "./js/script.js",
+  module: {
+    loaders: [
+      {
+      test: /\.js?$/, 
+      exclude: /(node_modules|bower_componets)/,
+      loader: 'babel-loader', 
+      query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
+        }
+      }
+    ]
+  },
   output: {
     path: __dirname + "/js",
-    filename: "scripts.min.js"
+    filename: "script.min.js"
   },
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
