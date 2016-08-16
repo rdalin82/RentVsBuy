0eaf660edef92d15ab39c5e398751aa6243d79e4
rdalin82 <Robert.Dalin@gmail.com>
2016-08-15 22:37:12 -0500

    working app mvp

--- js/componets/Container.js
+++ js/componets/Container.js
@@ -16,7 +16,11 @@ export default class Container extends React.Component {
       purchasePrice: "200000.00",
       interestRate: "3.25",
       taxes: "4500.00", 
-      downpayment: "10000.00"
+      downpayment: "10000.00", 
+      rentPayment: "1500.00", 
+      rentInsurance: "15.83", 
+      homeownerInsurance: "37.50",
+      assessments: "200.00"
     };
   }
   purchasePriceHandler(purchasePrice){
@@ -31,12 +35,44 @@ export default class Container extends React.Component {
   downpaymentHandler(downpayment){
     this.setState({downpayment});
   }
+  rentPaymentHandler(rentPayment){
+    this.setState({rentPayment})
+  }
+  rentInsuranceHandler(rentInsurance){
+    this.setState({rentInsurance});
+  }
+  homeownerInsuranceHandler(homeownerInsurance){
+    this.setState({homeownerInsurance});
+  }
+  assessmentsHandler(assessments){
+    this.setState({assessments});
+  }
   calculateTotalPayment(){
     const pp = parseFloat(this.state.purchasePrice);
     const dp = parseFloat(this.state.downpayment);
     const tax = parseFloat(this.state.taxes);
     const interest = parseFloat(this.state.interestRate);
-    return parseFloat(((pp-dp)/30/12)+(interest/12/100*pp)+(tax)/12).toFixed(2)
+    return ((pp-dp)/30/12)+(interest/12/100*pp)+(tax)/12
+  }
+  calculateFinalHomePayment(){
+    const assess = parseFloat(this.state.assessments);
+    const pmi = parseFloat(this.state.purchasePrice*0.01/12);
+    const mt = parseFloat(this.state.taxes/12);
+    const hi = parseFloat(this.state.homeownerInsurance);
+    return parseFloat(mt+hi+pmi+assess);
+  }
+  calculateRentPayment(){
+    const rp = parseFloat(this.state.rentPayment);
+    const ri = parseFloat(this.state.rentInsurance);
+    return parseFloat(rp+ri);
+  }
+  final(){
+    const total = this.calculateTotalPayment();
+    const fin = this.calculateFinalHomePayment();
+    console.log("total is " + total);
+    console.log("fin is " + fin);
+    console.log(total+fin)
+    return parseFloat(total+fin).toFixed(2);
   }
   render(){
     return (
@@ -70,7 +106,7 @@ export default class Container extends React.Component {
       <BlankLeftDiv 
         labelright="Total Payment" 
         dollarsignright="$" 
-        valueright={this.calculateTotalPayment()} />
+        valueright={parseFloat(this.calculateTotalPayment()).toFixed(2)} />
 }
       <TopHeader title="Rent vs Buy" />
 
@@ -79,11 +115,44 @@ export default class Container extends React.Component {
       <InnerDiv 
         labelleft="Monthly Rent Payment" 
         dollarsignleft="$" 
+        valueleft={this.state.rentPayment}
+        leftChangeHandler={this.rentPaymentHandler.bind(this)}
         labelright="Principal and Interest Payment" 
+        valueright={parseFloat(this.calculateTotalPayment()).toFixed(2)}
         dollarsignright="$" />
       
       <BlankLeftDiv 
         labelright="Estimated Monthly Taxes" 
+        valueright={parseFloat(this.state.taxes/12).toFixed(2)}
+        dollarsignright="$" />
+
+      <InnerDiv 
+        labelleft="Monthly Renter's Insurance" 
+        dollarsignleft="$" 
+        valueleft={this.state.rentInsurance}
+        leftChangeHandler={this.rentInsuranceHandler.bind(this)}
+        labelright="Estimated Homeowner's Insurance" 
+        valueright={this.state.homeownerInsurance}
+        rightChangeHandler={this.homeownerInsuranceHandler.bind(this)}
+        dollarsignright="$" />
+
+      <BlankLeftDiv 
+        labelright="Pirvate Mortgage Insurance"  
+        valueright={parseFloat(this.state.purchasePrice*0.01/12).toFixed(2)}
+        dollarsignright="$" />
+
+      <BlankLeftDiv 
+        labelright="Estimated Assessments"
+        valueright={this.state.assessments} 
+        rightChangeHandler={this.assessmentsHandler.bind(this)}
+        dollarsignright="$" />
+
+      <InnerDiv 
+        labelleft="Total Monthly Rent Payment" 
+        dollarsignleft="$" 
+        valueleft={this.calculateRentPayment()}
+        labelright="Total Monthly Purchase Payment"
+        valueright={this.final()}
         dollarsignright="$" />
      </div>
     );
