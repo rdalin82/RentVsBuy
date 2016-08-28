f4b4ec7d958709fb3f4cdadca9a90473dcebfd73
rdalin82 <Robert.Dalin@gmail.com>
2016-08-28 14:52:16 -0500

    changed spelling of component directory

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import Container from "./componets/Container"
+import Container from "./components/Container"
 
 const app = document.getElementById('app');
 
