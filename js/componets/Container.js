a965b24673db53aac593ae5ca3a3406c3e770a16
rdalin82 <Robert.Dalin@gmail.com>
2016-08-28 12:31:57 -0500

    added flux pattern to mortgage state

--- js/componets/Container.js
+++ js/componets/Container.js
@@ -4,6 +4,8 @@ import InnerDiv from "./InnerDiv";
 import BlankLeftDiv from "./BlankLeftDiv";
 import HalfDiv from "./HalfDiv";
 import InputBox from "./InputBox";
+import MortgageStore from "../stores/MortgageStore";
+import * as MortgageActions from "../actions/MortgageActions";
 
 var containerStyle = {
   margin: "0 10px 10px 0",
@@ -12,91 +14,87 @@ var containerStyle = {
 export default class Container extends React.Component {
   constructor(){
     super();
+    this.getMortgage = this.getMortgage.bind(this);
     this.state = {
-      purchasePrice: "200000.00",
-      interestRate: "3.25",
-      taxes: "4500.00", 
-      downpayment: "10000.00", 
-      rentPayment: "1500.00", 
-      rentInsurance: "15.83", 
-      homeownerInsurance: "37.50",
-      assessments: "200.00"
+      mortgage: MortgageStore.mortgageDetails(),
+      rental: {
+        rentPayment: "1500.00",
+        rentInsurance: "15.83"
+      }
     };
   }
-  purchasePriceHandler(purchasePrice){
-    this.setState({purchasePrice});
+  componentWillMount(){
+    MortgageStore.on("change", this.getMortgage);
   }
-  interestRateHandler(interestRate){
-    this.setState({interestRate});
+  componentWillUnMount(){
+    MortgageStore.removeListener("change", this.mortgageDetails);
   }
-  taxesHandler(taxes){
-    this.setState({taxes});
+  getMortgage(){
+    this.setState({
+      mortgage: MortgageStore.mortgageDetails(),
+    })
   }
-  downpaymentHandler(downpayment){
-    this.setState({downpayment});
+  reloadMortgage(){
+    MortgageActions.reloadMortgage();
   }
+
   rentPaymentHandler(rentPayment){
     this.setState({rentPayment})
   }
   rentInsuranceHandler(rentInsurance){
     this.setState({rentInsurance});
   }
-  homeownerInsuranceHandler(homeownerInsurance){
-    this.setState({homeownerInsurance});
-  }
-  assessmentsHandler(assessments){
-    this.setState({assessments});
-  }
   calculateTotalPayment(){
-    const pp = parseFloat(this.state.purchasePrice);
-    const dp = parseFloat(this.state.downpayment);
-    const tax = parseFloat(this.state.taxes);
-    const interest = parseFloat(this.state.interestRate);
+    const pp = parseFloat(this.state.mortgage.purchasePrice);
+    const dp = parseFloat(this.state.mortgage.downpayment);
+    const tax = parseFloat(this.state.mortgage.taxes);
+    const interest = parseFloat(this.state.mortgage.interestRate);
     return ((pp-dp)/30/12)+(interest/12/100*pp)
   }
   calculateFinalHomePayment(){
-    const assess = parseFloat(this.state.assessments);
-    const pmi = parseFloat(this.state.purchasePrice*0.01/12);
-    const mt = parseFloat(this.state.taxes/12);
-    const hi = parseFloat(this.state.homeownerInsurance);
+    const assess = parseFloat(this.state.mortgage.assessments);
+    const pmi = parseFloat(this.state.mortgage.purchasePrice*0.01/12);
+    const mt = parseFloat(this.state.mortgage.taxes/12);
+    const hi = parseFloat(this.state.mortgage.homeownerInsurance);
     return parseFloat(mt+hi+pmi+assess);
   }
   calculateRentPayment(){
-    const rp = parseFloat(this.state.rentPayment);
-    const ri = parseFloat(this.state.rentInsurance);
+    const rp = parseFloat(this.state.rental.rentPayment);
+    const ri = parseFloat(this.state.rental.rentInsurance);
     return parseFloat(rp+ri);
   }
-  final(){
+  finalCalc(){
     const total = this.calculateTotalPayment();
     const fin = this.calculateFinalHomePayment();
     return parseFloat(total+fin).toFixed(2);
   }
+  ////////
   render(){
     return (
      <div style={containerStyle}>
       <TopHeader title="Mortgage Details"/>
 
       <InnerDiv 
-        leftChangeHandler={this.purchasePriceHandler.bind(this)} 
-        rightChangeHandler={this.interestRateHandler.bind(this)}
         labelleft="Purchase Price" dollarsignleft="$" 
-        valueleft={this.state.purchasePrice}  
+        leftChangeHandler={MortgageActions.setPurchasePrice.bind(this)} 
+        valueleft={this.state.mortgage.purchasePrice} 
         labelright="APR Interest Rate"
-        valueright={this.state.interestRate} />
+        rightChangeHandler={MortgageActions.setInterestRate.bind(this)}
+        valueright={this.state.mortgage.interestRate} />
 
       <InnerDiv 
-        leftChangeHandler={this.downpaymentHandler.bind(this)}
-        valueleft={this.state.downpayment}
+        leftChangeHandler={MortgageActions.setDownpayment.bind(this)}
+        valueleft={this.state.mortgage.downpayment}
         labelleft="Down Payment" 
         labelright="Principal + Interest" 
         dollarsignleft="$" 
         dollarsignright="$"/>
 
       <InnerDiv 
-        rightChangeHandler={this.taxesHandler.bind(this)}
+        rightChangeHandler={MortgageActions.setTaxes.bind(this)}
         labelleft="Estimated Monthly PMI"
         labelright="Taxes" 
-        valueright={this.state.taxes}
+        valueright={this.state.mortgage.taxes}
         dollarsignleft="$" 
         dollarsignright="$"/>
 
@@ -112,7 +110,7 @@ export default class Container extends React.Component {
       <InnerDiv 
         labelleft="Monthly Rent Payment" 
         dollarsignleft="$" 
-        valueleft={this.state.rentPayment}
+        valueleft={this.state.rental.rentPayment}
         leftChangeHandler={this.rentPaymentHandler.bind(this)}
         labelright="Principal and Interest Payment" 
         valueright={parseFloat(this.calculateTotalPayment()).toFixed(2)}
@@ -120,28 +118,28 @@ export default class Container extends React.Component {
       
       <BlankLeftDiv 
         labelright="Estimated Monthly Taxes" 
-        valueright={parseFloat(this.state.taxes/12).toFixed(2)}
+        valueright={parseFloat(this.state.mortgage.taxes/12).toFixed(2)}
         dollarsignright="$" />
 
       <InnerDiv 
         labelleft="Monthly Renter's Insurance" 
         dollarsignleft="$" 
-        valueleft={this.state.rentInsurance}
+        valueleft={this.state.rental.rentInsurance}
         leftChangeHandler={this.rentInsuranceHandler.bind(this)}
         labelright="Estimated Homeowner's Insurance" 
-        valueright={this.state.homeownerInsurance}
-        rightChangeHandler={this.homeownerInsuranceHandler.bind(this)}
+        valueright={this.state.mortgage.homeownerInsurance}
+        rightChangeHandler={MortgageActions.setHomeownersInsurance.bind(this)}
         dollarsignright="$" />
 
       <BlankLeftDiv 
         labelright="Pirvate Mortgage Insurance"  
-        valueright={parseFloat(this.state.purchasePrice*0.005/12).toFixed(2)}
+        valueright={parseFloat(this.state.mortgage.purchasePrice*0.005/12).toFixed(2)}
         dollarsignright="$" />
 
       <BlankLeftDiv 
         labelright="Estimated Assessments"
-        valueright={this.state.assessments} 
-        rightChangeHandler={this.assessmentsHandler.bind(this)}
+        valueright={this.state.mortgage.assessments} 
+        rightChangeHandler={MortgageActions.setAssessments.bind(this)}
         dollarsignright="$" />
 
       <InnerDiv 
@@ -149,7 +147,7 @@ export default class Container extends React.Component {
         dollarsignleft="$" 
         valueleft={this.calculateRentPayment()}
         labelright="Total Monthly Purchase Payment"
-        valueright={this.final()}
+        valueright={this.finalCalc()}
         dollarsignright="$" />
      </div>
     );
---
+++ js/stores/MortgageStore.js
@@ -0,0 +1,75 @@
+import { EventEmitter } from 'events';
+import dispatcher from "../dispatcher"; 
+
+class MortgageStore extends EventEmitter { 
+  constructor(){
+    super()
+    this.mortgage = {
+      purchasePrice: "200000.00",
+      interestRate: "3.25",
+      taxes: "4500.00", 
+      downpayment: "10000.00",
+      homeownerInsurance: "37.50",
+      assessments: "200.00"
+    };
+  }
+  mortgageDetails(){
+    return this.mortgage;
+  }
+  setPurchasePrice(purchasePrice){
+    this.mortgage.purchasePrice=purchasePrice;
+  }
+  setInterestRate(interestRate){
+    this.mortgage.interestRate=interestRate;
+  }
+  setTaxes(taxes){
+    this.mortgage.taxes=taxes;
+  }
+  setDownpayment(downpayment){
+    this.mortgage.downpayment=downpayment;
+  }
+  setHomeownerInsurance(homeownerInsurance){
+    this.mortgage.homeownerInsurance=homeownerInsurance;
+  }
+  setAssessments(assessments){
+    this.mortgage.assessments=assessments;
+  }
+  handleActions(action){
+    switch(action.type){
+      case "PURCHASE_PRICE": {
+        this.setPurchasePrice(action.purchasePrice);
+        this.emit("change");
+        break; 
+      }
+      case "INTEREST_RATE":{
+        this.setInterestRate(action.interestRate);
+        this.emit("change");
+        break;
+      }
+      case "TAXES":{
+        this.setTaxes(action.taxes);
+        this.emit("change");
+        break;
+      }
+      case "DOWNPAYMENT":{
+        this.setDownpayment(action.downpayment);
+        this.emit("change");
+        break;
+      }
+      case "HOMEOWNER_INSURANCE":{
+        this.setHomeownerInsurance(action.homeownerInsurance);
+        this.emit("change");
+        break;
+      }
+      case "ASSESSMENTS": {
+        this.setAssessments(action.assessments);
+        this.emit("change");
+        break;
+      }
+    }
+  }
+}
+const mortgageStore = new MortgageStore;
+
+dispatcher.register(mortgageStore.handleActions.bind(mortgageStore));
+export default mortgageStore
